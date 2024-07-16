744465202590da72cf0fd1fd437d7583de70f074
kumar06062000 <166922331+kumar06062000@users.noreply.github.com>
2024-07-16 17:19:03 +0530

    Add files via upload

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('products/', views.products, name='products'),
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
@@ -18,4 +19,11 @@ urlpatterns = [
     path('admin_panel/vendor/edit/<int:vendor_id>/', views.edit_vendor, name='edit_vendor'),
     path('admin_panel/vendor/delete/<int:vendor_id>/', views.delete_vendor, name='delete_vendor'),
     path('admin_panel/dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('wishlist/', views.view_wishlist, name='view_wishlist'),
+    path('wishlist/add/<int:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
+    path('wishlist/remove/<int:product_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
+    
+    path('generate_excel_report/', views.generate_excel_report, name='generate_excel_report'),
+    path('update_notification_preference/', views.update_notification_preference, name='update_notification_preference'),
+    path('set_review_interval/', views.set_review_interval, name='set_review_interval'),
 ]
